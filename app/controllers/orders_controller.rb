cc681b4fdb864e6876e121269fc79d5e6b0b0f35
marty.bruck <marty.bruck@vineti.com>
2022-01-13 08:31:37 -0700

    completed workflow processing

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,80 @@
+class OrdersController < ApplicationController
+  LOCATIONS = %w[US Europe]
+  ORDER_TASK_NAME = 'Create Order'
+  ORDER_SUBMIT_TASK_NAME = 'Submit Order'
+  BASE_URL = 'http://localhost:8080/api/v1/'
+  PROCESS_INSTANCE_PATH = 'processinstance/'
+
+  before_action :check_for_cancel, only: [:create, :update]
+
+  def check_for_cancel
+    redirect_to root_path if params[:commit].eql?('Cancel')
+  end
+
+  def new
+    @order = Order.new
+    @process_instance_id = params[:process_instance_id]
+    @task_id = params[:task_id]
+    @locations = LOCATIONS
+  end
+
+  ##
+  # retrieve and validate the patient demographics. If ok, mark this task as complete
+  def create
+    @order = Order.new(params[:order].as_json)
+    @process_instance_id = params[:process][:process_instance_id]
+    @task_id = params[:process][:task_id]
+
+    if @order.valid?
+      url = "#{BASE_URL}#{PROCESS_INSTANCE_PATH}#{params[:process][:process_instance_id]}/completeTask/#{params[:process][:task_id]}"
+      begin
+        response = RestClient.patch(url, params[:order].to_json, :content_type => 'application/json')
+
+        if response.instance_values['code'].between?(200, 207)
+          redirect_to root_url, notice: "#{format('Successfully completed ', ORDER_TASK_NAME)} step."
+        else
+          @error = 'Unable to create'
+          render new_order_path
+        end
+      rescue StandardError => e
+        @error = "Unable to create new process instance:  #{e.message}"
+      end
+    else
+      render new_order_path
+    end
+  end
+  ##
+  # retrieve and display order information. If user confirms, update
+  def edit
+    @error = params[:error]
+    @process_instance_id = params[:id]
+    @task_id = params[:task_id]
+
+    # get the process instance variables to populate the current order
+    url = "#{BASE_URL}#{PROCESS_INSTANCE_PATH}#{@process_instance_id}/variables"
+    response = RestClient.get(url)
+    @variables = JSON.parse(response.body)
+    @order = Order.new()
+    @order.order_date = @variables['order_date']
+    @order.order_location = @variables['order_location']
+  end
+
+  def update
+    process_instance_id = params[:process][:process_instance_id]
+    task_id = params[:process][:task_id]
+    url = "#{BASE_URL}#{PROCESS_INSTANCE_PATH}#{process_instance_id}/completeTask/#{task_id}"
+    begin
+      response = RestClient.patch(url, params[:order].to_json, :content_type => 'application/json')
+
+      if response.instance_values['code'].between?(200, 207)
+        redirect_to root_path, notice: "#{format('Successfully completed ', ORDER_SUBMIT_TASK_NAME)} step."
+      else
+        @error = 'Unable to submit order'
+        redirect_to edit_order_path(process_instance_id, :task_id=> task_id)
+      end
+    rescue StandardError => e
+      @error = "Unable to create new process instance:  #{e.message}"
+      redirect_to edit_order_path(process_instance_id, :error => @error)
+    end
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+##
+#
+class PatientsController < ApplicationController
+  PATIENT_TASK_NAME = 'add_patient_demographics'
+  BASE_URL = 'http://localhost:8080/api/v1/'
+  PROCESS_INSTANCE_PATH = 'processinstance/'
+
+  before_action :check_for_cancel, only: [:create]
+
+  def check_for_cancel
+    redirect_to root_path if params[:commit].eql?('Cancel')
+  end
+
+  def new
+    @patient = Patient.new
+    @process_instance_id = params[:process_instance_id]
+    @task_id = params[:task_id]
+  end
+
+  ##
+  # retrieve and validate the patient demographics. If ok, mark this task as complete
+  def create
+    @patient = Patient.new(params[:patient].as_json)
+    @process_instance_id = params[:process][:process_instance_id]
+    @task_id = params[:process][:task_id]
+
+    if @patient.valid?
+      url = "#{BASE_URL}#{PROCESS_INSTANCE_PATH}#{params[:process][:process_instance_id]}/completeTask/#{params[:process][:task_id]}"
+      begin
+        response = RestClient.patch(url, params[:patient].to_json, :content_type => 'application/json')
+
+        if response.instance_values['code'].between?(200, 207)
+          redirect_to root_url, notice: "#{format('Successfully completed ', PATIENT_TASK_NAME)} step."
+        else
+          @error = 'Unable to create'
+          render new_patient_path
+        end
+      rescue StandardError => e
+        @error = "Unable to create new process instance:  #{e.message}"
+      end
+    else
+      render new_patient_path
+    end
+
+
+  end
+
+  # use this method to route to the specific update screen (e.g. add_patient_demographics, create_order, etc.)
+  # input is the selected workflow
+  def update
+    workflow = Workflow.new(p)
+  end
+end
